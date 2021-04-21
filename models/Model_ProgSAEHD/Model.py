086d06d3d1a0116202e9eef4b7741400a4fa1d7a
jh <jmhummel@gmail.com>
2021-04-21 13:46:53 -0700

    test8

--- models/Model_ProgSAEHD/Model.py
+++ models/Model_ProgSAEHD/Model.py
@@ -1215,7 +1215,7 @@ Examples: df, liae, df-d, df-ud, liae-ud, ...
                         else:
                             gpu_src_loss =  tf.reduce_mean ( 5*nn.dssim(gpu_target_src_masked_opt, gpu_pred_src_src_masked_opt, max_val=1.0, filter_size=int(resolution/11.6)), axis=[1])
                             gpu_src_loss += tf.reduce_mean ( 5*nn.dssim(gpu_target_src_masked_opt, gpu_pred_src_src_masked_opt, max_val=1.0, filter_size=int(resolution/23.2)), axis=[1])
-                    gpu_src_loss += 400 * tf.reduce_mean ( gaussian_weights[-1, :, :, :, :] * tf.abs ( gpu_target_src_masked_opt - gpu_pred_src_src_masked_opt ), axis=[1, 2, 3])
+                    gpu_src_loss += 400 * tf.reduce_mean ( tf.reduce_sum(gaussian_weights[-1, :, :, :, :] * tf.abs ( gpu_target_src_masked_opt - gpu_pred_src_src_masked_opt ), axis=[2, 3]), axis=[1])
 
                     if eyes_prio or mouth_prio:
                         if eyes_prio and mouth_prio:
@@ -1258,7 +1258,7 @@ Examples: df, liae, df-d, df-ud, liae-ud, ...
                         else:
                             gpu_dst_loss = tf.reduce_mean ( 5*nn.dssim(gpu_target_dst_masked_opt, gpu_pred_dst_dst_masked_opt, max_val=1.0, filter_size=int(resolution/11.6) ), axis=[1])
                             gpu_dst_loss += tf.reduce_mean ( 5*nn.dssim(gpu_target_dst_masked_opt, gpu_pred_dst_dst_masked_opt, max_val=1.0, filter_size=int(resolution/23.2) ), axis=[1])
-                    gpu_dst_loss += 400 * tf.reduce_mean ( gaussian_weights[-1, :, :, :, :] * tf.abs ( gpu_target_dst_masked_opt- gpu_pred_dst_dst_masked_opt ), axis=[1, 2, 3])
+                    gpu_dst_loss += 400 * tf.reduce_mean ( tf.reduce_sum(gaussian_weights[-1, :, :, :, :] * tf.abs ( gpu_target_dst_masked_opt- gpu_pred_dst_dst_masked_opt ), axis=[2, 3]), axis=[1])
 
 
                     if eyes_prio or mouth_prio:
